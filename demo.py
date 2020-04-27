5f75c89127c5e4e9a90ac12ea4d3e5936e80f894
bumfo <bumfo@users.noreply.github.com>
2020-04-27 13:44:26 +0800

    Add interesting demo

--- demo.py
+++ demo.py
@@ -2,14 +2,21 @@ from lazy import *
 
 def main():
   x = Var('x')
+  y = Var('y')
   f = Var('f')
-  f = Func('f', If(x > 1, x * f(x=x - 1), 1))
+  f = Func('f', If(x > 0, y + f(x=x - 1), 0))
 
   print(f(x=5))
-  print(f(x=5).eval())
-  print(f())
-  print(f().partial())
-  print(f().partial().eval(x=5))
+  print(f(x=5).partial())
+
+  print()
+
+  fact = Var('fact')
+  fact = Func('fact', If(x > 1, x * fact(x=x - 1), 1))
+
+  print(fact())
+  print(fact().partial())
+  print(fact().partial().eval(x=5))
 
 
 if __name__ == '__main__':
